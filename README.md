a317d9f47129e163aa9642065678969baf6de12f
Kalana <kalanas414@gmail.com>
2025-05-15 10:23:22 +0530

    add pages and route them and add navbar

---
+++ README.md
@@ -0,0 +1,23 @@
+## to run project
+
+npm install
+npm run dev
+
+## install packages
+
+npx @tailwindcss/upgrade 
+npm install react-router-dom
+
+## important things
+
+ * upgrade tailwindcsss v3 -> version v4 
+       v3.4.17 ->  ≈ tailwindcss v4.1.6
+
+
+npm install tailwindcss@latest postcss@latest autoprefixer@latest 
+        and remove postcss.config.js
+
+
+pause time : 45:40
+
+
---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Result from './pages/Result';
+import BuyCredit from './pages/BuyCredit';
+import Navbar from './components/NAvbar';
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-gray-200 to-gray-400'>
+
+<Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/buy" element={<BuyCredit />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App;
