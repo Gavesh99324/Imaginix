9dacc1a94f5fac519419f866f838fc4570626e01
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-26 04:35:01 +0530

    update nav bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
-import { FaStar } from "react-icons/fa";
-
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import StarOutlineIcon from '@mui/icons-material/StarOutline';
 
 
 function Navbar() {
@@ -21,11 +21,20 @@ function Navbar() {
       <div>
         {
         user ?   
-          <div>
-            <Button className='flex items-center gap-2'>
-                <FaStar className="text-yellow-500 w-4 h-4"/>
-                <p>Credits left :</p>
+          <div className='flex items-center gap-2 sm:gap-3'>
+            <Button variant='contained' color='primary' className='flex items-center gap-2 bg-gradient-to-r from-cyan-500/50 to-blue-500/50 text-white hover:from-cyan-500 to-blue-500'>
+                <StarOutlineIcon className="text-yellow-500 w-4 h-4"/>
+                <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits left :</p>
             </Button>
+            <p>Hi User </p>
+            <div className='relative group'>
+                <AccountCircleIcon className='cursor-pointer' onClick={() => navigate('/profile')} />
+                <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                    <ul>
+                        <li>Logout</li>
+                    </ul>
+                </div>  
+            </div>
           </div>
           :
           <div className='flex items-center gap-2 sm:gap-5'>
