73291c579fb3de7c024707b2dba55773cb857d46
Gavesh Jayasuriya <gaveshtishanj@gmail.com>
2025-05-26 12:25:01 +0530

    move all state variables and functions into context folder

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,16 +7,19 @@ import StarOutlineIcon from '@mui/icons-material/StarOutline';
 
 function Navbar() {
 
-    const [user, setUser] = useState(true);
+    const [user, setUser] = useState(null);
 
     const navigate = useNavigate();
 
 
   return (
     <div className='flex items-center justify-between py-4'>
-      <Link to={'/'}>
-         <img src={'/favicon.png'} alt="logo" className='mt-3 w-2 sm:w-10 lg:w-12' />
-      </Link>
+      <div className='flex items-center gap-2'>
+        <Link to={'/'}>
+          <img src={'/favicon.png'} alt="logo" className='mt-3 w-2 sm:w-10 lg:w-12' />
+        </Link>
+          <h2 className='text-lg sm-text-xl lg:text-2xl font-bold'>Imaginix</h2>
+      </div>
 
       <div>
         {
@@ -26,12 +29,12 @@ function Navbar() {
                 <StarOutlineIcon className="text-yellow-500 w-4 h-4"/>
                 <p className='text-xs sm:text-sm font-medium text-gray-600'>Credits left :</p>
             </Button>
-            <p>Hi User </p>
+            <p className='text-gray-600 max:sm:hidden pl-4'>Hi User </p>
             <div className='relative group'>
                 <AccountCircleIcon className='cursor-pointer' onClick={() => navigate('/profile')} />
                 <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
-                    <ul>
-                        <li>Logout</li>
+                    <ul className='list-none m-0 p-2 bg-white rounded-md border text-sm'>
+                        <li className='py-1 px-2 cursor-pointer pr-10'>Logout</li>
                     </ul>
                 </div>  
             </div>
@@ -39,7 +42,7 @@ function Navbar() {
           :
           <div className='flex items-center gap-2 sm:gap-5'>
             <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
-            <Button className='bg-gradient-to-r from-cyan-500 to-blue-500'>Login</Button>
+            <Button sx={{borderRadius: '9999px', width: '120px'}} className='bg-gradient-to-r from-cyan-500 to-blue-500 hover:from-cyan-500/50 hover:to-blue-500/50'>Login</Button>
           </div>
         }
       </div>
